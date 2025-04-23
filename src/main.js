bc4bf838122b9ada0964b2492bf17080ab485192
codest40 <generalcatmouse@gmail.com>
2025-04-23 16:40:29 +0200

    Making quotes display individually

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
-fetch('/quotes')
+fetch('https://api.quotable.io/quotes?limit=5')
   .then(res => res.json())
   .then(data => {
     const ul = document.getElementById('quote-list');
-    data.forEach(quote => {
+    data.results.forEach(quote => {
       const li = document.createElement('li');
-      li.textContent = `${quote.author}: "${quote.quote}"`;
+      li.textContent = `${quote.author}: "${quote.content}"`;
       ul.appendChild(li);
     });
+  })
+  .catch(err => {
+    console.error('Failed to load quotes:', err);
   });
