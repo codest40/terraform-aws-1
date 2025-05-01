552cc7e5399ed52950e54aa9c0f2d3ddead40ae2
codest40 <generalcatmouse@gmail.com>
2025-05-01 13:09:10 +0200

    Creating for Ansible

--- test.js
+++
@@ -1,9 +0,0 @@
-const assert = require('assert');
-
-const quotes = [
-  { id: 1, author: "Albert Einstein", quote: "Imagination is more important than knowledge." },
-  { id: 2, author: "Yoda", quote: "Do or do not. There is no try." }
-];
-
-assert.strictEqual(quotes.length, 2, 'There should be 2 quotes');
-console.log('✅ All tests passed!');
